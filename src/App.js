976f3729cef56b8abd039394a945aed953c51be2
Lily Best <lily.m.best@gmail.com>
2019-09-30 15:41:45 -0500

    base app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    moods: ['positive', 'negative'],
+    moodInput: '',
+    positive: ['Happy', 'Neutral', 'Manic', 'Calm'],
+    negative: ['Depressed', 'Sad', 'Fatigued', 'Angry', 'Lonely', 'Anxious', 'Mixed'],
+    date: Date,
+  }
+  handleAddMood = mood => {
+    this.setState({
+      moods : [mood, ...this.state.moods]
+    })
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    //state update
+    this.handleAddMood(this.state.moodInput)
+  }
+  handleOnChange = event => {
+    this.setState({
+      moodInput : event.target.value
+    })
+  }
+  render () {
+    const renderMoods = this.state.moodInput.map((item, index) => {
+      return (
+        <li key={index} className="mood">
+          {item}
+        </li>
+      )
+    })
+    return (
+      <div className="App">
+        <ul>{this.state.moodInput}</ul>
+        <form className="add-form">
+          <input 
+          type="text" 
+          onChange={this.handleOnChange} 
+          value={this.state.moodInput} 
+          autoComplete="off" 
+          />
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
