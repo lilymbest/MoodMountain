1db353737385869e836ecd8af66849cccf454a09
Lily Best <lily.m.best@gmail.com>
2019-10-01 17:30:53 -0500

    added buttons for theme changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Home.css'
 
 class Home extends Component{
     constructor(props){
@@ -9,28 +10,85 @@ class Home extends Component{
             'images/forestthemeicon.png', 
             'images/glacierthemeicon.png',
             'images/lavenderthemeicon.png',
-            'images/rainbowthemeicon.png'
+            'images/rainbowthemeicon.png',
+            'images/MoodMountain.png'
             ],
     theme: 'images/MoodMountain.png',
     }
-    this.changeThemes = this.changeThemes.bind(this);
+    this.autumnTheme = this.autumnTheme.bind(this)
+    this.forestTheme = this.forestTheme.bind(this)
+    this.glacierTheme = this.glacierTheme.bind(this)
+    this.lavenderTheme = this.lavenderTheme.bind(this)
+    this.rainbowTheme = this.rainbowTheme.bind(this)
+    this.defaultTheme = this.defaultTheme.bind(this)
 }
-changeThemes(){
+autumnTheme(){
     var images = this.state.images
-    var image = images[Math.floor(Math.random() * images.length)]
-    this.setState({theme:image})
+    var autumn = this.state.images[0]
+    this.setState({theme:autumn})
 }
 
-handleClick(img){
-    this.changeThemes(img)
-    return img
+forestTheme(){
+    var images = this.state.images
+    var forest = this.state.images[1]
+    this.setState({theme:forest})
 }
+glacierTheme(){
+    var images = this.state.images
+    var glacier = this.state.images[2]
+    this.setState({theme:glacier})
+}
+lavenderTheme(){
+    var images = this.state.images
+    var lavender = this.state.images[3]
+    this.setState({theme:lavender})
+}
+rainbowTheme(){
+    var images = this.state.images
+    var rainbow = this.state.images[4]
+    this.setState({theme:rainbow})
+}
+defaultTheme(){
+    var images = this.state.images
+    var defaultTheme = this.state.images[5]
+    this.setState({theme:defaultTheme})
+}
+
+handleClickAutumn(){
+    this.autumnTheme()
+}
+
+handleClickForest(){
+    this.forestTheme()
+}
+
+handleClickGlacier(){
+    this.glacierTheme()
+}
+
+handleClickLavender(){
+    this.lavenderTheme()
+}
+
+handleClickRainbow(){
+    this.rainbowTheme()
+}
+
+handleClickDefault(){
+    this.defaultTheme()
+}
+
 
 render() {
     return (
     <div className="home">
-        <button onClick={() => this.handleClick()}>Change Theme</button>
         <img src={this.state.theme} alt=""/>
+        <button onClick={() => this.handleClickAutumn()}><img className="btn" src="images/autumnthemeicon.png" /></button>
+        <button onClick={() => this.handleClickForest()}><img className="btn" src="images/forestthemeicon.png" /></button>
+        <button onClick={() => this.handleClickGlacier()}><img className="btn" src="images/glacierthemeicon.png" /></button>
+        <button onClick={() => this.handleClickLavender()}><img className="btn" src="images/lavenderthemeicon.png" /></button>
+        <button onClick={() => this.handleClickRainbow()}><img className="btn" src="images/rainbowthemeicon.png" /></button>
+        <button onClick={() => this.handleClickDefault()}><img className="btn" src="images/MoodMountain.png" /></button>
     </div>
     )
     }
