20c62871eef438bf9ace223c5e8b85ef129f32bb
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-02 14:30:10 -0500

    link moodinput and themecolor pages to home page

---
+++ src/pages/MoodInput/MoodInput.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './MoodInput.css';
+import { Route, Switch, Link } from 'react-router-dom';
+import ThemeColors from '../ThemeColors/ThemeColors'
+
+
+class App extends Component {
+  state = {
+    moods: ['Euphoric', 'Elevated', 'Neutral', 'Tense', 'Low'],
+    euphoric: ['Manic', 'Grandiose', 'Impulsive'],
+    elevated: ['Happy', 'Motivated', 'Excited'],
+    neutral: ['Calm', 'Unsure', 'Disassociated'],
+    tense: ['Anxious', 'Stressed', 'Angry'],
+    low: ['Sad', 'Lonely', 'Depressed'],
+    themes: {
+    riverbed: ['#D0EFB1', '#B3D89C', '#9DC3C2', '#77A6B6', '#4D7298'],
+    rainbow: ['#FDE74C', '#9BC53D', '#FA7921', '#E55934', '#5BC0EB'],
+    glacier: ['#A6E1FA', '#0E6BA8', '#0A2472', '#001C55', '#00072D'],
+    lavenderFields:['#EAFDF8', '#E5E9EC',' #DDCAD9', '#D1B1CB', '#7C616C'],
+    autumn: ['#C32F27', '#D8572A', '#DB7C26', '#F7B538', '#780116'],
+    forest: ['#DDFCAD', '#C8E087',' #95A472', '#82846D', '#646165']
+    },
+  }
+  render () {
+    return (
+      <Switch>
+         
+      <div className="App">
+      <Route exact path='/MoodInput' render={() =>
+      <>
+        <header className='feelingToday'>How are you feeling today?</header>
+
+        <ul>{this.state.moods.map(e => e.split(' ').map(f => <span> {f} </span>))}</ul>
+
+        <form className="add-form">
+        <input className="inputField"
+          type="text" 
+          onChange={this.handleOnChange} 
+          value={this.state.moodInput} 
+          autoComplete="off" 
+          />
+          <input type="submit" />
+          </form>
+
+			<div>
+
+      <Link className='btnTheme' to='/themecolors'>Change Theme</Link>
+			</div>
+
+        </>
+      } />
+        <Route exact path='/themecolors' render={() => 
+          <ThemeColors />
+          } />
+          
+      </div>
+     </Switch>
+    )
+  }
+}
+
+export default App;
+
