31d63c5088aeed22410aad9625863f57a1d87002
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-06 23:37:16 -0500

    format top nav bar and add icons to bottom nav bar

--- src/components/NavBarBottom/NavBarBottom.jsx
+++ src/components/NavBarBottom/NavBarBottom.jsx
@@ -6,8 +6,11 @@ import './NavBarBottom.css'
 const NavBarBottom = (props) => {
     return (
       <div className='NavBarBottom' style={{backgroundColor: '#D17B88'}}>
-        <img src="" alt=""/>
-        <Link to='/dashboard' className='NavBar-link'>Dashboard</Link>
+        <Link to='/landing'><img className="appIcon" src="images/appIcons/black_calendar.svg" alt=""/></Link>
+        <Link to='/dashboard'><img className="appIcon" src="images/appIcons/black_stats.svg" alt=""/></Link>
+        <Link to='/landing'><img className="appIconCenter" src="images/appIcons/black_plus_circle.svg" alt=""/></Link>
+        <Link to='/landing'><img className="appIcon" src="images/appIcons/black_profile.svg" alt=""/></Link>
+        <Link to='/landing'><img className="appIcon" src="images/appIcons/black_settings2.svg" alt=""/></Link>
       </div>
     );
   };
