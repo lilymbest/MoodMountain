a5b94a5efc07476f930ec3e928654c426b5d55b9
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-07 21:26:06 -0500

    center button and fix label size

--- src/components/MoodIcons/MoodIcons.jsx
+++ src/components/MoodIcons/MoodIcons.jsx
@@ -12,23 +12,23 @@ class MoodIcons extends Component{
                 <div className="moodSelector">
                     <div className="mood">
                         <img className="moodIcon" src={this.props.euphoricIcon} alt=""/>
-                        <h3>Euphoric</h3>
+                        <h3 className="moodLabel">Euphoric</h3>
                         </div> 
                     <div className="mood">
                         <img className="moodIcon" src={this.props.happyIcon} alt=""/>
-                        <h3>Elevated</h3>
+                        <h3 className="moodLabel">Elevated</h3>
                         </div> 
                     <div className="mood">
                         <img className="moodIcon" src={this.props.neutralIcon} alt=""/>
-                        <h3>Neutral</h3>
+                        <h3 className="moodLabel">Neutral</h3>
                         </div> 
                      <div className="mood">
                         <img className="moodIcon" src={this.props.tenseIcon} alt=""/>
-                        <h3>Tense</h3>
+                        <h3 className="moodLabel">Tense</h3>
                         </div> 
                     <div className="mood">
                         <img className="moodIcon" src={this.props.sadIcon} alt=""/>
-                        <h3>Low</h3>
+                        <h3 className="moodLabel">Low</h3>
                         </div> 
                     </div>
 
