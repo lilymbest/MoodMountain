e0b05f7ee96742a6eb41628f6b00566e39bdfae0
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-07 21:46:35 -0500

    center nav bar bottom

--- src/components/NavBarBottom/NavBarBottom.jsx
+++ src/components/NavBarBottom/NavBarBottom.jsx
@@ -5,7 +5,7 @@ import './NavBarBottom.css'
 
 const NavBarBottom = (props) => {
     return (
-      <div className='NavBarBottom' style={{backgroundColor: '#D17B88'}}>
+      <div className='NavBarBtm' style={{backgroundColor: '#D17B88'}}>
         <Link to='/landing'><img className="appIcon" src="images/appIcons/black_calendar.svg" alt=""/></Link>
         <Link to='/dashboard'><img className="appIcon" src="images/appIcons/black_stats.svg" alt=""/></Link>
         <Link to='/howareyou'><img className="appIconCenter" src="images/appIcons/black_plus_circle.svg" alt=""/></Link>
