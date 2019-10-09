cb72dad0263901044296e4a9b8d798c0ceaf4ff5
Lily Best <lily.m.best@gmail.com>
2019-10-09 12:15:07 -0500

    commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useCreateIndex', true)
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true,  useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://lilymbest:D92thl09@cluster0-5taha.mongodb.net/moodmountain?retryWrites=true&w=majority', {useNewUrlParser: true});
+         mongoose.connection.once('open', function(){
+         console.log('Conection has been made!');
+             }).on('error', function(error){
+          console.log('Error is: ', error);
+           });
+
+
 
 const db = mongoose.connection;
 
