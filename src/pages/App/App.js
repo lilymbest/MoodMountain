4f3532a43e8e372b6cd59f8acd35a896bcaa0941
Lily Best <lily.m.best@gmail.com>
2019-10-09 12:07:45 -0500

    commit

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,27 +1,25 @@
-/* Imports */
+/* Dependencies */
 import React, { Component } from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
+import { directive, tsImportEqualsDeclaration } from '@babel/types'
+import userService from '../../utils/userService';
+
 /* Components */
 import NavBarTop from '../../components/NavBarTop/NavBarTop'
 import NavBarBottom from '../../components/NavBarBottom/NavBarBottom'
+import MoodIcons from '../../components/MoodIcons/MoodIcons'
 
 /* Pages */
 import Dashboard from '../Dashboard/Dashboard'
 import Settings from '../Settings/Settings'
+import Login from '../Login/Login'
+import SignUp from '../SignUp/SignUp'
+import Landing from '../Landing/Landing'
+import Land from '../Land/Land'
+import Calendar from '../Calendar/Calendar'
+
 /* Styles */
-
 import './App.css';
-import { Route, Switch, Link } from 'react-router-dom';
-import MoodIcons from '../../components/MoodIcons/MoodIcons'
-import { directive, tsImportEqualsDeclaration } from '@babel/types';
-import Theme from '../../components/Theme/Theme'
-import userService from '../../utils/userService';
-import Login from '../Login/Login';
-import SignUp from '../SignUp/SignUp';
-import MoodMountain from '../../components/MoodMountain/MoodMountain';
-import Landing from '../Landing/Landing';
-import Land from '../Land/Land';
-import Calendar from '../Calendar/Calendar';
-
 
 
 class App extends Component {
@@ -29,143 +27,47 @@ class App extends Component {
     super()
     this.state = {
       user: userService.getUser(),
-      moods: ['Euphoric', 'Elevated', 'Neutral', 'Tense', 'Low'],
-      images: ['images/autumnthemeicon.png', 
-              'images/forestthemeicon.png', 
-              'images/glacierthemeicon.png',
-              'images/lavenderthemeicon.png',
-              'images/rainbowthemeicon.png',
-              'images/MoodMountain.png'
-              ],
-      backgroundColors: ['#C32F27', '#646165', '#0E6BA8', '#DDCAD9', '#9BC53D', '#75F4F4'],
-      themeIcon: '',
-      themeBackground: '#75F4F4',
-      euphoricIcon: 'images/Icons/Default/euphoricDefault2.png',
-      happyIcon: 'images/Icons/Default/happyDefault.png',
-      neutralIcon: 'images/Icons/Default/neutralDefault.png',
-      tenseIcon: 'images/Icons/Default/tenseDefault.png',
-      sadIcon: 'images/Icons/Default/zsadDefault.png',
-      }
-      this.autumnTheme = this.autumnTheme.bind(this)
-      this.forestTheme = this.forestTheme.bind(this)
-      this.glacierTheme = this.glacierTheme.bind(this)
-      this.lavenderTheme = this.lavenderTheme.bind(this)
-      this.rainbowTheme = this.rainbowTheme.bind(this)
-      this.defaultTheme = this.defaultTheme.bind(this)
-      this.handleClickAutumn = this.handleClickAutumn.bind(this)
+    }
+    /* Binding (this) to functions */
       this.handleLogout = this.handleLogout.bind(this)
       this.handleSignupOrLogin = this.handleSignupOrLogin.bind(this)
   }
-  autumnTheme(){
-    var autumnEphoricIcon = 'images/Icons/Autumn/euphoricAutumn.png'
-    var autumnHappyIcon = 'images/Icons/Autumn/happyAutumn.png'
-    var autumnNeutralIcon = 'images/Icons/Autumn/neutralAutumn.png'
-    var autumnTenseIcon = 'images/Icons/Autumn/tenseAutumn.png'
-    var autumnSadIcon = 'images/Icons/Autumn/zsadAutumn.png'
-    var autumn = this.state.images[0]
-    var autumnBackground = this.state.backgroundColors[0]
-    this.setState({themeIcon:autumn})
-    this.setState({themeBackground:autumnBackground})
-    this.setState({euphoricIcon: autumnEphoricIcon})
-    this.setState({happyIcon: autumnHappyIcon})
-    this.setState({neutralIcon: autumnNeutralIcon})
-    this.setState({tenseIcon: autumnTenseIcon})
-    this.setState({sadIcon: autumnSadIcon})
-}
 
-forestTheme(){
-    var forestEphoricIcon = 'images/Icons/Forest/euphoricForest.png'
-    var forestHappyIcon = 'images/Icons/Forest/happyForest.png'
-    var forestNeutralIcon = 'images/Icons/Forest/neutralForest.png'
-    var forestTenseIcon = 'images/Icons/Forest/tenseForest.png'
-    var forestSadIcon = 'images/Icons/Forest/zsadForest.png'
-    var forest = this.state.images[1]
-    var forestBackground = this.state.backgroundColors[1]
-    this.setState({themeIcon:forest})
-    this.setState({themeBackground:forestBackground})
-    this.setState({euphoricIcon: forestEphoricIcon})
-    this.setState({happyIcon: forestHappyIcon})
-    this.setState({neutralIcon: forestNeutralIcon})
-    this.setState({tenseIcon: forestTenseIcon})
-    this.setState({sadIcon: forestSadIcon})
-}
-glacierTheme(){
-    var glacier = this.state.images[2]
-    var glacierBackground = this.state.backgroundColors[2]
-    this.setState({themeIcon:glacier})
-    this.setState({themeBackground:glacierBackground})
+  handleLogout = () => {
+    userService.logout();
+    this.setState({ user: null });
+  }
 
-}
-lavenderTheme(){
-    var lavender = this.state.images[3]
-    var lavenderBackground = this.state.backgroundColors[3]
-    this.setState({themeIcon:lavender})
-    this.setState({themeBackground:lavenderBackground})
-}
-rainbowTheme(){
-    var rainbow = this.state.images[4]
-    var rainbowBackground = this.state.backgroundColors[4]
-    this.setState({themeIcon:rainbow})
-    this.setState({themeBackground:rainbowBackground})
-}
-defaultTheme(){
-    var defaultTheme = this.state.images[5]
-    var defaultBackground = this.state.backgroundColors[5]
-    this.setState({themeIcon:defaultTheme})
-    this.setState({themeBackground:defaultBackground})
-}
-
-handleClickAutumn(){
-    this.autumnTheme()
-    console.log(this.state.euphoricIcon)
-}
-
-handleClickForest(){
-    this.forestTheme()
-}
-
-handleClickGlacier(){
-    this.glacierTheme()
-}
-
-handleClickLavender(){
-    this.lavenderTheme()
-}
-
-handleClickRainbow(){
-    this.rainbowTheme()
-}
-
-handleClickDefault(){
-    this.defaultTheme()
-}
-
-handleLogout = () => {
-  userService.logout();
-  this.setState({ user: null });
-}
-
-handleSignupOrLogin = () => {
-  this.setState({user: userService.getUser()});
-}
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
 
   render() {
     return (
       <div className="App" style={{}}>
-        <header></header>
         <NavBarTop
         user={this.state.user}
         handleLogout={this.handleLogout}
         />
-
-        <Switch>
-
-        <Route exact path='/land' render={() => <Land /> } />
-        <Route exact path='/landing' render={() => <Landing /> } />
-        <Route exact path='/calendar' render={() => <Calendar /> } />
-        <Route exact path='/settings' render={() => <Settings /> } />
-        <Route exact path='/dashboard' render={() => <Dashboard user={this.state.user}/> } />
-        <Route exact path='/signup' render={({ history }) => 
+        <Switch>  
+          <Route exact path='/' render={() => 
+            <Land /> 
+            }/>
+          <Route exact path='/landing' render={() => 
+            <Landing /> 
+          }/>
+          <Route exact path='/calendar' render={() => 
+            <Calendar /> 
+          }/>
+          <Route exact path='/settings' render={() => 
+            <Settings /> 
+          }/>
+          <Route exact path='/dashboard' render={() => 
+            <Dashboard 
+            user={this.state.user}
+            /> 
+          }/>
+          <Route exact path='/signup' render={({ history }) => 
             <SignUp
               history={history}
               handleSignupOrLogin={this.handleSignupOrLogin}
@@ -177,21 +79,18 @@ handleSignupOrLogin = () => {
               handleSignupOrLogin={this.handleSignupOrLogin}
             />
           }/>
-        <Route exact path='/howareyou' render={() =>
-        <>
-        <Theme 
-        themeIcon={this.state.themeIcon}
-        />
-        <MoodIcons
-        euphoricIcon={this.state.euphoricIcon}
-        happyIcon={this.state.happyIcon}
-        neutralIcon={this.state.neutralIcon}
-        tenseIcon={this.state.tenseIcon}
-        sadIcon={this.state.sadIcon}
-        handleClickAutumn={this.handleClickAutumn}
-        />
-        </>
-        } />
+          <Route exact path='/howareyou' render={() =>
+            <>
+              <MoodIcons
+                euphoricIcon={this.state.euphoricIcon}
+                happyIcon={this.state.happyIcon}
+                neutralIcon={this.state.neutralIcon}
+                tenseIcon={this.state.tenseIcon}
+                sadIcon={this.state.sadIcon}
+                handleClickAutumn={this.handleClickAutumn}
+              />
+          </>
+        }/>
         </Switch>
         <NavBarBottom 
         user={this.state.user}
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,67 +1,64 @@
+/* Dependencies */
 import React from 'react'
 import ReactDOM from 'react-dom';
-import './Dashboard.css';
 import ReactApexChart from 'react-apexcharts'
-import ApexCharts from 'apexcharts'
 import userService from '../../utils/userService';
 import { thisTypeAnnotation } from '@babel/types';
+/* Styles */
+import './Dashboard.css';
 
 
 class DonutChart extends React.Component {
     constructor(props) {
-    super(props);
-    let user = userService.getUser()
-    let userTheme = user.themeColors
-    this.state = {
-        user: userService.getUser(),
-        months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-        month: "October",
-        options: {
-        labels: ["Euphoric", "Happy", "Neutral", "Tense", "Sad"],
-        colors: userTheme,
-          responsive: [{
-            breakpoint: 480,
+        super(props);
+        let user = userService.getUser()
+        let userTheme = user.themeColors
+        this.state = {
+            user: userService.getUser(),
+            months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+            month: "October",
             options: {
-              chart: {
-                width: 200,
-                type: DonutChart,
-              },
-              legend: {
-                position: 'bottom',
-                
-              }
-            }
-          }]
-        },
-        series: [20, 20, 20, 20, 20]
-    } 
-}
+                labels: ["Euphoric", "Happy", "Neutral", "Tense", "Sad"],
+                colors: userTheme,
+                responsive: [{
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200,
+                            type: DonutChart,
+                        },
+                        legend: {
+                            position: 'bottom',    
+                        }
+                    }
+                }]
+            },
+            series: [20, 20, 20, 20, 20]
+        } 
+    }
 
 
-handleLastClick() {
-    this.setState({month:"September"})
-}
-handleNextClick(){
-    this.setState({month:"November"})
-}
-render() {
-    return (
-    <div>
-      <div className="moodChartHeader">
-         <button onClick={() => this.handleLastClick()}> <img className="arrows" src="images/appIcons/black_arrow_left.svg"></img></button>
-         <h1>{this.state.month}</h1>
-         <button onClick={() => this.handleNextClick()}> <img className="arrows" src="images/appIcons/black_arrow_right.svg"></img></button>
-         </div>
-      <div className="moodChart" onLoad={() => this.setState({colors: this.state.user.themeColors}), console.log(this.state.user.themeColors)}>
-        <ReactApexChart options={this.state.options} series={this.state.series} type="donut" width="380" user={this.state.user} colors={this.state.colors} /> 
-      </div>
-        <div id="html-dist">
+    handleLastClick() {
+        this.setState({month:"September"})
+    }
+    handleNextClick(){
+        this.setState({month:"November"})
+    }
+    render() {
+        return (
+        <div>
+            <div className="moodChartHeader">
+                <button onClick={() => this.handleLastClick()}> <img className="arrows" src="images/appIcons/black_arrow_left.svg"></img></button>
+                    <h1>{this.state.month}</h1>
+                <button onClick={() => this.handleNextClick()}> <img className="arrows" src="images/appIcons/black_arrow_right.svg"></img></button>
+            </div>
+            <div className="moodChart" onLoad={() => this.setState({colors: this.state.user.themeColors}), console.log(this.state.user.themeColors)}>
+                <ReactApexChart options={this.state.options} series={this.state.series} type="donut" width="380" user={this.state.user} colors={this.state.colors} /> 
+            </div>
         </div>
-    </div>
       );
     }
-  }
+}
 
 
-  
-  export default DonutChart
+export default DonutChart
