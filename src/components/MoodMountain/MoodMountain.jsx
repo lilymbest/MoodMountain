0a0b227f21bc87ad01d1c183c1dfc872fde475dc
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-10-10 01:24:33 -0500

    center logo and tagline

--- src/components/MoodMountain/MoodMountain.jsx
+++ src/components/MoodMountain/MoodMountain.jsx
@@ -4,11 +4,11 @@ import './MoodMountain.css'
 class Logo extends Component{
     render(){
     return(
-    <div className="logoMM">
-      <img className="logoImage" alt="logo" src="images/MoodMountain.png" />
-      <h1 className="logoName">Mood Mountain </h1>
-      <h3 className="logoTagline">Emotional Tracking</h3>
-    </div>
+      <div className="logoMM">
+        <img className="logoImage" alt="logo" src="images/MoodMountain.png" />
+        <h1 className="logoName">Mood Mountain </h1>
+        <h3 className="logoTagline">Emotional Tracking</h3>
+      </div>
     )
 }
 }
